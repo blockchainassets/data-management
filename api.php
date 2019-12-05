f46f4fb2f4f09ce7cf4d64a69637f649c3d1377f
BlockchainAssets <38862960+blockchainassets@users.noreply.github.com>
2019-12-05 13:37:02 +0100

    Add files via upload
    Nomics Cryptocurrency & Bitcoin API
    
    Herramientas usadas para el consume del API:
        • XAMPP 
        • Visual Studio Code
        • PHP
    Para poder realizar la petición y consumo del API, se solicitó una clave en la cual se me fue proporcionada por medio de correo electrónico, existen muchas herramientas y forma de consumos del API, incluyendo el formato de representación de datos.
    
    La url para consumir los datos correspondientes fue la siguiente con su clave correspondiente: https://api.nomics.com/v1/currencies/ticker?key=6903383d34a9e34c22628ee87e17f0e3
    
    Existen varios tipos de recursos para consumir, lo cual en nuestro caso usamos Curriences Ticker para extraer los datos que se requieren. 
    
     
    
    Para corroborar que la información a consumir fuera la correcta, se ingresó esta url en el navegador.
    
    Dentro del archivo api.php anexado, se encuentra el código documentado de los procesos que se realizó para consumir el API, explicando de manera breve. Siendo la siguiente imagen el resultado obtenido:
    El resultado se actualiza cada 1 minuto y se ve reflejado en el precio.

---
+++ api.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <!-- La siguiente linea de código nos permitirá actualizar la página cada 60 segundos,
+         lo que equivale a 1 minuto, lo cual realizará la petición consecutiva de nuestro código en php -->
+    <meta http-equiv="refresh" content="60">
+</head>
+
+<body>
+<?php    
+//Para el consumo del API, se optó realizarlo con CURL, concatenando la url con la clave proporcionada del 
+//siguiente sitio web: https://p.nomics.com/cryptocurrency-bitcoin-api
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.nomics.com/v1/currencies/ticker?key=6903383d34a9e34c22628ee87e17f0e3");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// $output contendrá el resultado de la petición //La respuesta en JSON // Al mismo tiempo ejecutamos la petición
+$output = curl_exec($ch);
+// Cerrarmos curl para liberar recursos del sistema
+curl_close($ch);   
+//Interpretamos el resultado JSON para facilitar la manipulación de los datos
+$array = json_decode($output, true);
+//Se realiza un recorrido o ciclo de toda la información obtenida. 
+foreach ($array as $value) {
+        //Se pregunta si el id sea referente a la moneda Bitcoin
+        //En caso que lo sea se muestra en pantalla el nombre de moneda y su precio
+        if ($value['id'] == "BTC")
+        {
+                $cadena = "Nombre de moneda : ".$value['name'];
+                echo "<br>";
+                echo "Extraer el precio de la moneda Bitcoin cada 1 minuto";
+                echo "<br>";
+                echo "<br>";
+                print ($cadena);
+                echo "<br>";
+                echo "Precio: ".$value['price'];
+                echo "<br>";
+        }
+}
+
+//NOTA: Para realizar este mismo procedimiento pueden ser de diferentes formas, usando Ajax, Jquery y/o Javascript
+//Se crear un time para ejecutar la petición Curl encapsulado a una función cada 1 minuto y de esa manera se van registrando los precios 
+//de cripto moneda en la misma página 
+
+?>
+</body>
+</html>
